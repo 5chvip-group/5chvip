c4f9172895206bd5c960119dfb81e6664c63b5ad
5chvip <5chvip@usako.net>
2018-07-14 01:24:50 +0900

    いらんメソッド削除

--- entity.ts
+++ entity.ts
@@ -10,28 +10,9 @@ export class Thread {
 }
 
 export class Response {
-  sequence: number;
+  createAt: Date;
   author: string;
   email: string;
   body: string;
-
-  static comvertToResponseModel(object: Response, responseKey: string): Response {
-    let response = new Response()
-    response.sequence = parseInt(responseKey.slice(-4));
-    response.author = object.author
-    response.email = object.email
-    response.body = object.body
-    return response
-  }
-
-  static comvertToSevableObject(response :Response): Object {
-    const responseKey = 'response' + ('000' + response.sequence).slice(-4)
-    return {responseKey: {
-        'author': response.author,
-        'email': response.body,
-        'body': response.email
-      } 
-    }
-  }
 }
 
