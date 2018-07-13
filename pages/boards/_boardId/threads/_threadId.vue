87ec74e529800539a0ccbccff6cb81a3bcbfef87
5chvip <5chvip@usako.net>
2018-07-14 01:35:02 +0900

    firebase のところコメントアウト

--- pages/boards/_boardId/threads/_threadId.vue
+++ pages/boards/_boardId/threads/_threadId.vue
@@ -9,33 +9,45 @@
 </template>
 
 <script lang="ts">
-import { Response } from "~/entity";
-import firebase from '~/plugins/firebase'
-
-export default {
-  data () {
-    return {
-      responses: []
-    }
-  },
-  created () {
-    this.listen();
-  },
-  methods: {
-    listen () {
-      // const boardId = this.$route.params.boardId
-      // const threadId = this.$route.params.threadId
-      const boardId = 'e0VynIX6fezdCiYav6f0'
-      const threadId = '4PkfuM9eNNjzER6uNn4z'
-      const query = firebase.firestore().collection('boards').doc(boardId).collection('threads').doc(threadId).collection('responses').orderBy('createAt')
-      query.onSnapshot(querySnapshot => {
-        const responses: Response[] = []
-        querySnapshot.forEach(doc => {
-          responses.push(doc.data() as Response)
-        });
-        this.responses = responses
-      })
-    }
-  }
+import {
+  Component,
+  Vue
+} from "nuxt-property-decorator"
+import { Getter, State } from "vuex-class"
+@Component({})
+export default class extends Vue {
+  @Getter getThreadById
+  @Getter getResponsesByThreadId
 }
+
+
+// import { Response } from "~/entity";
+// import firebase from '~/plugins/firebase'
+
+// export default {
+//   data () {
+//     return {
+//       responses: []
+//     }
+//   },
+//   created () {
+//     this.listen();
+//   },
+//   methods: {
+//     listen () {
+//       // const boardId = this.$route.params.boardId
+//       // const threadId = this.$route.params.threadId
+//       const boardId = 'e0VynIX6fezdCiYav6f0'
+//       const threadId = '4PkfuM9eNNjzER6uNn4z'
+//       const query = firebase.firestore().collection('boards').doc(boardId).collection('threads').doc(threadId).collection('responses').orderBy('createAt')
+//       query.onSnapshot(querySnapshot => {
+//         const responses: Response[] = []
+//         querySnapshot.forEach(doc => {
+//           responses.push(doc.data() as Response)
+//         });
+//         this.responses = responses
+//       })
+//     }
+//   }
+// }
 </script>
