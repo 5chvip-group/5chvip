9331bc15f79c866c7ccbc7ae42ac65a1ff2ef57a
5chvip <5chvip@usako.net>
2018-07-15 00:10:39 +0900

    リアルタイム取得に対応

--- pages/boards/_boardId/threads/_threadId.vue
+++ pages/boards/_boardId/threads/_threadId.vue
@@ -3,8 +3,7 @@
     <h1 class="header">{{ $store.getters.getThreadById($route.params.threadId).title }}</h1>
     めざせ1000レス
     <ul>
-      <li v-for="response in $store.getters.getResponsesByThreadId($route.params.threadId)">{{ response.body }}</li>
-      <!-- <li v-for="response in responses">{{ response.body }}</li> -->
+      <li v-for="response in responses">{{ response.body }}</li>
       <ResForm :threadId="$route.params.threadId"></ResForm>
     </ul>
   </section>
@@ -17,45 +16,49 @@ import {
 } from "nuxt-property-decorator"
 import { Getter, State } from "vuex-class"
 import ResForm from '~/components/ResForm.vue'
+import { Response } from "~/entity";
+import firebase from '~/plugins/firebase'
 
 @Component({
   components: {
     ResForm
+  },
+  created() {
+    this.listen();
   }
 })
 export default class extends Vue {
   @Getter getThreadById
-  @Getter getResponsesByThreadId
+  responses: Response[]
+  asyncData () {
+    // const boardId = this.$route.params.boardId
+    // const threadId = this.$route.params.threadId
+    const boardId = 'e0VynIX6fezdCiYav6f0'
+    const threadId = '4PkfuM9eNNjzER6uNn4z'
+    const query = firebase.firestore().collection('boards').doc(boardId).collection('threads').doc(threadId).collection('responses').orderBy('createAt')
+    return query.get()
+      .then(snapshot => {
+        const responses: Response[] = []
+        snapshot.forEach(doc => {
+          responses.push(doc.data() as Response)
+        });
+        return {responses: responses}
+      })
+  }
+  listen () {
+    // const boardId = this.$route.params.boardId
+    // const threadId = this.$route.params.threadId
+    const boardId = 'e0VynIX6fezdCiYav6f0'
+    const threadId = '4PkfuM9eNNjzER6uNn4z'
+    const query = firebase.firestore().collection('boards').doc(boardId).collection('threads').doc(threadId).collection('responses').orderBy('createAt')
+    query.onSnapshot(querySnapshot => {
+      const responses: Response[] = []
+      querySnapshot.forEach(doc => {
+        responses.push(doc.data() as Response)
+      });
+      this.responses = responses
+    })
+  }
 }
 
-
-// import { Response } from "~/entity";
-// import firebase from '~/plugins/firebase'
-
-// export default {
-//   data () {
-//     return {
-//       responses: []
-//     }
-//   },
-//   created () {
-//     this.listen();
-//   },
-//   methods: {
-//     listen () {
-//       // const boardId = this.$route.params.boardId
-//       // const threadId = this.$route.params.threadId
-//       const boardId = 'e0VynIX6fezdCiYav6f0'
-//       const threadId = '4PkfuM9eNNjzER6uNn4z'
-//       const query = firebase.firestore().collection('boards').doc(boardId).collection('threads').doc(threadId).collection('responses').orderBy('createAt')
-//       query.onSnapshot(querySnapshot => {
-//         const responses: Response[] = []
-//         querySnapshot.forEach(doc => {
-//           responses.push(doc.data() as Response)
-//         });
-//         this.responses = responses
-//       })
-//     }
-//   }
-// }
 </script>
